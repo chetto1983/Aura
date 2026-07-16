952aeec0ac8e9e94da7eea6cbf16cb3486f49b55
davide marchetto <dvdmarchetto@gmail.com>
2026-07-17 01:44:26 +0200

    build(web): refresh final Calm Prism bundle

--- internal/webui/dist/assets/chevron-down-BoZTQ750.js
+++
@@ -1,1 +0,0 @@
-import{x as e}from"./authConfig-D8kS3NH7.js";var t=e(`chevron-down`,[[`path`,{d:`m6 9 6 6 6-6`,key:`qrunsl`}]]);export{t};
--- internal/webui/dist/assets/chevron-right-DnAEY3hS.js
+++
@@ -1,1 +0,0 @@
-import{x as e}from"./authConfig-D8kS3NH7.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
